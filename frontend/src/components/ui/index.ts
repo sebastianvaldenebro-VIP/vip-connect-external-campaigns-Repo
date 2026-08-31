564a9a11cc3988afc756da5ceadda18fdbabb1b3
Sebastian Valdenebro <sebastian.valdenebro@medrowk.io>
2026-08-31 10:17:34 -0500

    feat(frontend): barrel-export the new Monitor UI primitives

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './status';
+export * from './StatusChip';
+export * from './ProgressBar';
+export * from './StepStrip';
+export * from './StatTile';
+export * from './Avatar';
+export * from './ActivityFeed';
